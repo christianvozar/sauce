c93478085834b0de3dd0a364b21f6b7fe411556c
Christian R. Vozar <christian@rogueethic.com>
2017-07-18 14:07:31 -0500

    versioning

---
+++ version.go
@@ -0,0 +1,16 @@
+// Copyright © 2017
+// MIT Licensed.
+// Created by Christian R. Vozar <christian@rogueethic.com> ⚜
+
+package sauce
+
+var (
+	// Version is ...
+	Version = "1.0.0"
+
+	// SpecificationVersion is the ACiD SAUCE specification version.
+	SpecificationVersion = "00"
+
+	// SpecificationRevision is the ACiD SAUCE specification revision.
+	SpecificationRevision = "5"
+)
